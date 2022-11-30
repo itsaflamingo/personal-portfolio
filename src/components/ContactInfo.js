135315523aed3e5b4f5d379fae2d8426ada3cbf3
Liz <elizabeth.pinero.r@gmail.com>
2022-11-30 17:54:10 -0500

    Styled contact section.

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -4,24 +4,26 @@ export default function ContactInfo() {
             <h1>Contact me</h1>
             <p>Please get in touch if you think our work could be mutually beneficial!</p>
 
-            <div id='address'>
-                <p>215 Renforth Dr</p>
-                <p>Toronto, ON, Canada</p>
-            </div>
-
-            <div id='contact'>
-                <div id='phone'>
-                    <p>647-515-2632</p>
+            <div id="personal-info">
+                <div id='address'>
+                    <p>215 Renforth Dr</p>
+                    <p>Toronto, ON, Canada</p>
                 </div>
-                <div id='email'>
-                    <p>elizabeth.r.pinero@gmail.com</p>
-                </div>
-            </div>
 
-            <div id='footer-social'>
-                <i className="devicon-linkedin-plain colored"></i>
-                <i className="devicon-github-original colored"></i>
-                <i className="devicon-twitter-original"></i>
+                <div id='contact'>
+                    <div id='phone'>
+                        <p>647-515-2632</p>
+                    </div>
+                    <div id='email'>
+                        <p>elizabeth.r.pinero@gmail.com</p>
+                    </div>
+                </div>
+
+                <div id='footer-social'>
+                    <i className="devicon-linkedin-plain colored"></i>
+                    <i className="devicon-github-original colored"></i>
+                    <i className="devicon-twitter-original"></i>
+                </div>
             </div>
         </div>
     )
