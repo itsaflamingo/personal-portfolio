3e04d2e640c0ac527b3527ce0aa27bb87d6f2367
Liz <elizabeth.pinero.r@gmail.com>
2022-11-30 22:26:16 -0500

    Updated UI, headings now match.

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -3,7 +3,7 @@ import SocialIcons from "./SocialIcons";
 export default function ContactInfo() {
     return (
         <div id='contact-info'>
-            <h1>Contact me</h1>
+            <h3>Contact me</h3>
             <p>Please get in touch if you think our work could be mutually beneficial!</p>
 
             <div id="personal-info">
