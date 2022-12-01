335916a1bd6c6cd1c58b5d63eb8990b1a8f79c9a
Liz <elizabeth.pinero.r@gmail.com>
2022-12-01 15:12:20 -0500

    Updated 'About me' section.

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,9 +5,9 @@ export default function AboutMe() {
     return (
         <div id='header'>
             <div id='about-me'>
-                <h3>About</h3>
+                <h3>About me</h3>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor ornare lacinia. Quisque scelerisque maximus dui quis tempus. Vestibulum semper gravida mi, sed pretium enim aliquam nec. Praesent et finibus ipsum. Morbi nisi lorem, lacinia nec aliquet at, pulvinar eget urna. Curabitur hendrerit rutrum lorem, eu vulputate mi scelerisque at. Nullam vel nisl viverra, semper enim non, eleifend diam. Curabitur ullamcorper ante sapien, sed auctor sem eleifend quis. Cras suscipit dolor nec vulputate congue. 
+                    My name is Elizabeth, and I am a front-end, soon to be full-stack, software engineer. I have always been passionate about technology and the potential for creativity that has emerged as a result. I have spent the past four years working in pastry, where I fell in love with creating beautiful, detailed and functional desserts. During the COVID lockdowns I co-started a pastry start-up, and found myself really enjoying the process of creating a website. I decided to learn more about web development by creating projects. As I continued to create I slowly fell in love with web development, and decided to pursue software development as a career.  
                 </p>
             </div>
             <SocialIcons />
