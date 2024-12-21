1dfddb2d03af41f6a5e44dd654b37ed1d82f1124
Elizabeth Pinero <elizabeth.pinero.r@gmail.com>
2024-12-21 11:02:54 -0500

    now mobile-friendly

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,15 @@
-import '../styles/App.css';
-import Personal from './Personal';
-import Projects from './Projects';
-import ContactInfo from './ContactInfo'
+import "../styles/App.css";
+import Personal from "./Personal";
+import Projects from "./Projects";
+import ContactInfo from "./ContactInfo";
 
 function App() {
-
   return (
-    <div className="App">
-      <div id='main'>
-          <Personal />
-          <Projects />
-          <ContactInfo />
+    <div className="w-screen flex flex-col gap-10">
+      <div id="main">
+        <Personal />
+        <Projects />
+        <ContactInfo />
       </div>
     </div>
   );
--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -2,7 +2,7 @@ import SocialIcons from "./SocialIcons";
 
 export default function ContactInfo() {
   return (
-    <div className="bg-[#0991B1] w-full text-black mt-20 pb-1 flex flex-col items-center justify-center p-2">
+    <div className="bg-[#0991B1] w-screen text-black mt-20 pb-1 flex flex-col items-center justify-center p-2">
       <h3 className="text-2xl font-merriweather font-semibold text-mbLite">
         Contact
       </h3>
